c35c64830b3aea4c0a7c7d44a139b961543805ff
Kevin Heard <kevin.heard@travelctm.com>
2020-05-05 15:33:32 +1000

    likes & comments

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require('apollo-server');
+const { AuthenticationError, UserInputError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -38,6 +38,10 @@ module.exports = {
 
       const post = await newPost.save();
 
+      context.pubsub.publish('NEW_POST', {
+        newPost: post
+      });
+
       return post;
     },
 
@@ -56,6 +60,35 @@ module.exports = {
       } catch(err) {
         throw new Error(err);
       }
+    },
+
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          // Post already liked, unlike it
+          post.likes = post.likes.filter(like => like.username !== username);
+        } else {
+          // not liked, like post
+          post.likes.push({
+            username,
+            createdAt: (new Date()).toISOString()
+          });
+        }
+
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError('Post not fonud');
+      }
+    }
+  },
+
+  Subscription: {
+    newPost: {
+      subscribe: (_, __, { pubsub }) => pubsub.asyncIterator('NEW_POST')
     }
   }
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,16 +2,33 @@ const { gql } = require('apollo-server');
 
 module.exports = gql`
   type Post {
-    id: ID!,
-    body: String!,
-    createdAt: String!,
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+    comments: [Comment]!
+    likes: [Like]!
+    likeCount: Int!
+    commentCount: Int!
+  }
+
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+
+  type Like {
+    id: ID!
+    createdAt: String!
     username: String!
   }
 
   type User{
     id: ID!,
-    email: String!,
-    token: String!,
+    email: String!
+    token: String!
     username: String!
     createdAt: String!
   }
@@ -28,10 +45,17 @@ module.exports = gql`
     getPost(postId: ID!): Post
   }
 
-  type Mutation{
+  type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: String!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+
+  type Subscription {
+    newPost: Post!
   }
 `;
